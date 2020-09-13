aea754ce222e08b2041acd431a65349d809df706
Andreas Kocman <andreas.kocman@gmail.com>
2020-09-13 21:35:05 +0200

    Fixed mappings for CustomerRelationship classes

--- customer_service/src/main/java/at/technikum/masterproject/customernetwork/model/mapper/CustomerRelationshipMapper.java
+++ customer_service/src/main/java/at/technikum/masterproject/customernetwork/model/mapper/CustomerRelationshipMapper.java
@@ -3,12 +3,19 @@ package at.technikum.masterproject.customernetwork.model.mapper;
 import at.technikum.masterproject.customernetwork.model.CustomerRelationship;
 import at.technikum.masterproject.customernetwork.model.dto.CustomerRelationshipDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CustomerRelationshipMapper {
 
+  @Mapping(source = "id.sourceCustomerId", target = "sourceCustomerId")
+  @Mapping(source = "id.targetCustomerId", target = "targetCustomerId")
+  @Mapping(source = "id.relationshipType", target = "relationshipType")
   CustomerRelationshipDto customerRelationshipToCustomerRelationshipDto(CustomerRelationship customerRelationship);
 
+  @Mapping(source = "sourceCustomerId", target = "id.sourceCustomerId")
+  @Mapping(source = "targetCustomerId", target = "id.targetCustomerId")
+  @Mapping(source = "relationshipType", target = "id.relationshipType")
   CustomerRelationship customerRelationshipDtoToCustomerRelationship(CustomerRelationshipDto customerRelationshipDto);
 
 }

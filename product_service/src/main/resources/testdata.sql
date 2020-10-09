0376b7ff64736c8fbf2a22602471d43e73044de8
Andreas Kocman <andreas.kocman@gmail.com>
2020-10-09 13:20:41 +0200

    Edited sql formatting for product service testdata

--- product_service/src/main/resources/testdata.sql
+++ product_service/src/main/resources/testdata.sql
@@ -1,68 +1,131 @@
-
 -- Default Products
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
-INSERT INTO product (name, description, weight, width, height, depth) VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
+INSERT INTO product (name, description, weight, width, height, depth)
+VALUES ('Foo', 'Bar', 3.5, 3, 4, 5);
 
 -- Default product reviews
-INSERT INTO product_review (customer_id, product_id, stars, review) VALUES (1, 1, 1, 'bad');
-INSERT INTO product_review (customer_id, product_id, stars, review) VALUES (2, 1, 3, 'meh');
-INSERT INTO product_review (customer_id, product_id, stars, review) VALUES (3, 1, 5, 'good');
-INSERT INTO product_review (customer_id, product_id, stars, review) VALUES (1, 2, 5, 'superb');
+INSERT INTO product_review (customer_id, product_id, stars, review)
+VALUES (1, 1, 1, 'bad');
+INSERT INTO product_review (customer_id, product_id, stars, review)
+VALUES (2, 1, 3, 'meh');
+INSERT INTO product_review (customer_id, product_id, stars, review)
+VALUES (3, 1, 5, 'good');
+INSERT INTO product_review (customer_id, product_id, stars, review)
+VALUES (1, 2, 5, 'superb');

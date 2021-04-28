fda9bb8698c3699ade3106e1c1d05d7f4ecdf605
kocmana <andreas.kocman@gmail.com>
2021-04-28 11:32:57 +0200

    Code Cleanup: Removed RecommenderService because it is now out of scope.

--- recommender_service/src/main/java/at/technikum/masterproject/transactionservice/recommendation/RecommendationRepository.java
+++
@@ -1,9 +0,0 @@
-package at.technikum.masterproject.transactionservice.recommendation;
-
-import at.technikum.masterproject.transactionservice.recommendation.model.RecommendationEntity;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecommendationRepository extends ReactiveCrudRepository<RecommendationEntity, Long> {
-}

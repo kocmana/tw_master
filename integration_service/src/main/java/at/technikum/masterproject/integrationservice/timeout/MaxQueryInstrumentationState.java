1bf7843d05fc440cf3982c87e387863544926299
Andreas Kocman <andreas.kocman@gmail.com>
2021-05-01 16:52:46 +0200

    Simplified QueryDurationInstrumentation

--- integration_service/src/main/java/at/technikum/masterproject/integrationservice/timeout/MaxQueryInstrumentationState.java
+++ integration_service/src/main/java/at/technikum/masterproject/integrationservice/timeout/MaxQueryInstrumentationState.java
@@ -1,32 +1,17 @@
 package at.technikum.masterproject.integrationservice.timeout;
 
 import graphql.execution.instrumentation.InstrumentationState;
-import lombok.NoArgsConstructor;
-import org.springframework.util.StopWatch;
 
-@NoArgsConstructor
 public class MaxQueryInstrumentationState implements InstrumentationState {
 
-  private final StopWatch stopWatch = new StopWatch();
+  private final long startTime;
+
+  public MaxQueryInstrumentationState(){
+    startTime = System.currentTimeMillis();
+  }
 
   public Long getTime() {
-    return stopWatch.getTotalTimeMillis();
-  }
-
-  public void startStopWatch() {
-    synchronized (stopWatch) {
-      if (!stopWatch.isRunning()) {
-        stopWatch.start();
-      }
-    }
-  }
-
-  public void stopStopWatch() {
-    synchronized (stopWatch) {
-      if (stopWatch.isRunning()) {
-        stopWatch.stop();
-      }
-    }
+    return System.currentTimeMillis() - startTime;
   }
 
 }
